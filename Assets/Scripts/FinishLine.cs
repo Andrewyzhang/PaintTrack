8269e28b2da2bccf56455426639aec66b456657e
Andrew Zhang <andrew34zhang@gmail.com>
2021-11-08 00:24:58 -0500

    Level finish popup

--- Assets/Scripts/FinishLine.cs
+++ Assets/Scripts/FinishLine.cs
@@ -1,12 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class FinishLine : MonoBehaviour
 {
+  public GameObject Endscrn;
   private void OnTriggerEnter2D(Collider2D collision)
   {
-
+    if (collision.gameObject.tag == "Player")
+    {
+      Endscrn.gameObject.SetActive(true);
+    }
   }
 }
