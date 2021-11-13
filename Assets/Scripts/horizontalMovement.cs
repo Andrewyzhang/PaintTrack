e9fdc45c0a120d79d1250f12508766876fee4d37
Michael Ngai <michael.ngai@tufts.edu>
2021-11-13 10:40:53 -0500

    switched movement to using animator instead of a script

--- Assets/Scripts/horizontalMovement.cs
+++ Assets/Scripts/horizontalMovement.cs
@@ -8,16 +8,21 @@ public class horizontalMovement : MonoBehaviour
     void Update()
     {
 
+      bool left = false;
+
       float movespeed = 0.0f;
 
-      if (movespeed < 0) {
+      if (left) {
         movespeed -= .01f;
       } else {
         movespeed += .01f;
       }
 
-      if (movespeed > .1 || movespeed < -.1) {
-        movespeed = -1 * movespeed;
+      if (movespeed > 10) {
+        left = true;
+      } else if (movespeed < -10) {
+        left = false;
+        //test
       }
 
       transform.position = new Vector3(transform.position.x + movespeed, transform.position.y);
