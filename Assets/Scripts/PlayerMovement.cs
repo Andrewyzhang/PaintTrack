9070eb6d11b6c6d431ef2b904b88e11bac72a2f2
Andrew Zhang <andrew34zhang@gmail.com>
2021-11-14 16:21:10 -0500

    Added audio manager

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,41 +4,44 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    
-    public CharacterController2D controller;
-    
-    public float runSpeed = 40f;
-    
-    bool jump = false;
-    
-    float horizontalM = 0f;
-    
-    
-    
-    // Start is called before the first frame update
-    void Start()
+
+  public CharacterController2D controller;
+
+  public float runSpeed = 40f;
+
+  bool jump = false;
+
+  float horizontalM = 0f;
+
+
+
+  // Start is called before the first frame update
+  void Start()
+  {
+
+  }
+
+
+
+  // Update is called once per frame
+  void Update()
+  {
+
+    horizontalM = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+    if (Input.GetButtonDown("Jump"))
     {
-        
+      jump = true;
+      FindObjectOfType<AudioManager>().Play("Jump");
     }
 
 
+  }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-        horizontalM = Input.GetAxisRaw("Horizontal") * runSpeed;
-        
-        if (Input.GetButtonDown("Jump")) {
-            jump = true;
-        }
-        
-        
-    }
-    
-    
-    void FixedUpdate(){
-        controller.Move(horizontalM * Time.fixedDeltaTime, false, jump);
-        jump = false;
-    }
+
+  void FixedUpdate()
+  {
+    controller.Move(horizontalM * Time.fixedDeltaTime, false, jump);
+    jump = false;
+  }
 }
