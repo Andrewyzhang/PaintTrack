a86bc9c10d7a4933484e4ba4ae95d15794a80cb1
Michael Ngai <michael.ngai@tufts.edu>
2021-12-05 15:11:47 -0500

    flag animations work

--- Assets/Scripts/checkpoint.cs
+++ Assets/Scripts/checkpoint.cs
@@ -8,10 +8,10 @@ public class checkpoint : MonoBehaviour
 
     public Animator anim;
 
-    void Start() {
-      anim = GetComponent<Animator>();
-      anim.enabled = false;
-    }
+    // void Start() {
+    //   anim = GetComponent<Animator>();
+    //   anim.enabled = false;
+    // }
 
     void OnTriggerEnter2D(Collider2D other)
     {
@@ -19,12 +19,14 @@ public class checkpoint : MonoBehaviour
 
         if (other.tag == "Player") // filter the objects that collide with the checkpoint. You can assign the tag in the inspector
         {
+          anim.SetBool("reached", true);
+          anim.SetBool("waving", true);
           // Grab the X and Y values of the checkpoint position
           pointupdate = new Vector2( transform.position.x,transform.position.y);
           // Update the _respawnPoint varaible in GameManager
           GameHandler.respawnPoint = pointupdate;
 
-          anim.enabled = true;
+          // anim.enabled = true;
 
           // anim.Play("checkpointReached");
           // anim.Play("checkpointWaving");
