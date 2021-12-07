9ae0793f009769224cc0c124ae4c86359b7345eb
Andrew Zhang <andrew34zhang@gmail.com>
2021-12-06 21:00:23 -0500

    Added color switch sound

--- Assets/Scripts/SwitchCubes.cs
+++ Assets/Scripts/SwitchCubes.cs
@@ -4,67 +4,69 @@ using UnityEngine;
 
 public class SwitchCubes : MonoBehaviour
 {
-    public GameObject Player;
-    public GameObject blueCube, redCube;
-    public ParticleSystem redParticles, blueParticles;
+  public GameObject Player;
+  public GameObject blueCube, redCube;
+  public ParticleSystem redParticles, blueParticles;
 
-    int whichCubeIsOn = 1;
-    public static string playerColor;
-    // Start is called before the first frame update
-    void Start()
+  int whichCubeIsOn = 1;
+  public static string playerColor;
+  // Start is called before the first frame update
+  void Start()
+  {
+    blueCube.gameObject.SetActive(true);
+    redCube.gameObject.SetActive(false);
+    playerColor = "blue";
+    Player.layer = 9;
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+
+    if (Input.GetKeyDown(KeyCode.X))
     {
+      SwitchCube();
+      FindObjectOfType<AudioManager>().Play("Switch", true);
+    }
+
+  }
+
+  public void SwitchCube()
+  {
+    switch (whichCubeIsOn)
+    {
+      // if blue is on
+      case 1:
+
+        //Particles
+        blueParticles.Play();
+
+        // the red is on
+        whichCubeIsOn = 2;
+
+        // disable blue one and enable red
+        blueCube.gameObject.SetActive(false);
+        redCube.gameObject.SetActive(true);
+        playerColor = "red";
+        Player.layer = 8;
+        break;
+
+      // if red is on
+      case 2:
+        // the blue is on
+
+        //Particles
+        redParticles.Play();
+
+        whichCubeIsOn = 1;
+
+        // disable red one and enable blue
         blueCube.gameObject.SetActive(true);
         redCube.gameObject.SetActive(false);
         playerColor = "blue";
         Player.layer = 9;
+        break;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-
-         if(Input.GetKeyDown(KeyCode.X)) {
-              SwitchCube();
-         }
-
-    }
-
-    public void SwitchCube()
-    {
-        switch (whichCubeIsOn)
-        {
-            // if blue is on
-            case 1:
-
-                //Particles
-                blueParticles.Play();
-
-                // the red is on
-                whichCubeIsOn = 2;
-
-                // disable blue one and enable red
-                blueCube.gameObject.SetActive(false);
-                redCube.gameObject.SetActive(true);
-                playerColor = "red";
-                Player.layer = 8;
-                break;
-
-            // if red is on
-            case 2:
-				// the blue is on
-
-                //Particles
-                redParticles.Play();
-
-                whichCubeIsOn = 1;
-
-                // disable red one and enable blue
-                blueCube.gameObject.SetActive(true);
-                redCube.gameObject.SetActive(false);
-                playerColor = "blue";
-                Player.layer = 9;
-                break;
-        }
-
-    }
+  }
 }
