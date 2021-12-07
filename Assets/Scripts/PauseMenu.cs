5fc61efed86956d073849a81d6fb2a5e8e443bee
afrogeo <afroditi.georgiadi@tufts.edu>
2021-12-07 16:11:05 -0500

    fixed background for all scenes and updated pause menu

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -8,7 +8,7 @@ public class PauseMenu : MonoBehaviour
     public static bool IsPaused = false;
     public GameObject pauseMenuUI;
 
-
+    void Start() { Time.timeScale = 1; }
     // Update is called once per frame
     void Update()
     {
